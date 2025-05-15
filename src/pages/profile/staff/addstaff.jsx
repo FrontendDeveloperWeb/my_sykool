e45d379fdd605752132d59e4274dbe88584af0db
Noman Waheed <114811212+FrontendDeveloperWeb@users.noreply.github.com>
2025-05-16 02:26:54 +0500

    Update addstaff.jsx

--- src/pages/profile/staff/addstaff.jsx
+++ src/pages/profile/staff/addstaff.jsx
@@ -7,8 +7,8 @@ import { DollarOutlined, FileTextOutlined, InfoCircleOutlined, LinkOutlined, Loc
 import ProfileForm from '@/components/partial/staffform/profileform';
 import IncreamentHistoryForm from '@/components/partial/staffform/increamenthistoryform';
 import PayRoll from '@/components/partial/staffform/payroll';
-import SecurityDeposit from '@/components/partial/staffform/securitydeposit';
-import LoanAdvance from '@/components/partial/staffform/loaaadvanceform';
+import SecurityDeposit from '@/components/partial/staffform/securityeposit';
+import LoanAdvance from '@/components/partial/staffform/loanadvanceform';
 import StaffDocumentsForm from '@/components/partial/staffform/staffdocumentsform';
 
 const items = [
